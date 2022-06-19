c37622d8ac18b3bf80129319baadc0027bfb6542
J-SilvaJunior <mokozuma@gmail.com>
2022-06-19 12:59:04 -0300

    19/06

--- Game1.cs
+++ Game1.cs
@@ -9,9 +9,12 @@ namespace Game1Game
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Color cl = Color.Black;
-        Texture2D shuttle;
-        Texture2D stars;
-        Texture2D earth;
+        private Texture2D shuttle;
+        private Texture2D stars;
+        private Texture2D earth;
+        private SpriteFont font;
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -32,6 +35,7 @@ namespace Game1Game
             shuttle = Content.Load<Texture2D>("shuttle");
             stars = Content.Load<Texture2D>("stars");
             earth = Content.Load<Texture2D>("earth");
+            font = Content.Load<SpriteFont>("testFont");
             // TODO: use this.Content to load your game content here
         }
 
@@ -75,7 +79,19 @@ namespace Game1Game
             _spriteBatch.Draw(shuttle,
                               new Vector2(450,240),
                               Color.White);
+            //Vector2 centroTexto = font.MeasureString("Olá mundo!") / 2;
+
+            Vector2 posicaoCentral = new Vector2(Window.ClientBounds.Width  / 2,
+                                                 Window.ClientBounds.Height / 2);
             
+            _spriteBatch.DrawString(font,
+                                    "Olá Mundo!",
+                                    posicaoCentral,
+                                    Color.White,
+                                    0,
+                                    new Vector2(0,0),
+                                    1.0f,
+                                    SpriteEffects.None,0.5f);
             
             
             _spriteBatch.End();
