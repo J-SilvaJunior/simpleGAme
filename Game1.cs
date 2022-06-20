56f994c902e2571b7785aa7db1d9ca7971f92ee8
J-SilvaJunior <mokozuma@gmail.com>
2022-06-20 00:46:07 -0300

    20/06

--- Game1.cs
+++ Game1.cs
@@ -6,14 +6,17 @@ namespace Game1Game
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private Color cl = Color.Black;
-        private Texture2D shuttle;
-        private Texture2D stars;
-        private Texture2D earth;
-        private SpriteFont font;
+        
+        private GraphicsDeviceManager _graphics;    //Gerenciador de gráficos
+        private SpriteBatch _spriteBatch;           //Desenhador de Sprite
 
+        private Color cl = Color.Black; //CL = Cor, utilizei para alterar, em testes, a cor da tela
+        private Texture2D shuttle;      //É a variável de Textura 2D para a Nave
+        private Texture2D stars;        //É a variável de Textura 2D para o Background Estrelado
+        private Texture2D earth;        //É a variável de Textura 2D para a terra
+        private SpriteFont font;        //
+        private Vector2 shuttlePos;     //
+        private float shuttleSpd;   //
 
         public Game1()
         {
@@ -25,7 +28,7 @@ namespace Game1Game
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            shuttleSpd = 1000f;
             base.Initialize();
         }
 
@@ -47,19 +50,43 @@ namespace Game1Game
                 Exit();
 
             // TODO: Add your update logic here
-            /*
-            if (kState.IsKeyDown(Keys.Space))
-                cl = Color.White;
-            if (kState.IsKeyDown(Keys.G))
-                cl = Color.Green;
+
+            //if (kState.IsKeyDown(Keys.Space))
+
+
+            if (kState.IsKeyDown(Keys.W))
+                ShuttleMove('w', gameTime);
+            
             if (kState.IsKeyDown(Keys.S))
-                cl = Color.Silver;
-            if (kState.IsKeyDown(Keys.R))
-                cl = Color.Red;
-            if (kState.IsKeyDown(Keys.B))
-                cl = Color.Blue;
-            */
+                ShuttleMove('s', gameTime);
+
+            if (kState.IsKeyDown(Keys.A))
+                ShuttleMove('a', gameTime);
+
+            if (kState.IsKeyDown(Keys.D))
+                ShuttleMove('d', gameTime);
+
             base.Update(gameTime);
+        }
+
+        void ShuttleMove(char key,GameTime gameTime)
+        {
+            switch (key)
+            {
+                case 'w':
+                    shuttlePos.Y -= shuttleSpd * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;                
+                case 'a':
+                    shuttlePos.X -= shuttleSpd * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case 's':
+                    shuttlePos.Y += shuttleSpd * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case 'd':
+                    shuttlePos.X += shuttleSpd * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+
+            }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -75,14 +102,34 @@ namespace Game1Game
             _spriteBatch.Draw(earth, 
                               new Vector2(400,240),
                               Color.White);
-            
-            _spriteBatch.Draw(shuttle,
-                              new Vector2(450,240),
-                              Color.White);
-            Vector2 centroTexto = font.MeasureString("Ola mundo!") / 2;
+            Vector2 centroTexto = font.MeasureString("Ola Mundo!") / 2;
 
             Vector2 posicaoCentral = new Vector2(Window.ClientBounds.Width  / 2,
                                                  Window.ClientBounds.Height / 2);
+
+            
+            _spriteBatch.Draw(shuttle,
+                              shuttlePos,
+                              null,
+                              Color.White,
+                              1,
+                              new Vector2(shuttle.Width / 2, shuttle.Height / 2), 
+                              Vector2.One,
+                              SpriteEffects.None,
+                              0f); 
+            /*
+            _spriteBatch.Draw(shuttle,
+                              new Rectangle((int)posicaoCentral.X,
+                                            (int)posicaoCentral.Y,
+                                            shuttle.Width,
+                                            shuttle.Height),
+                              null,
+                              Color.White,
+                              0.9f,
+                              new Vector2(shuttle.Width / 2,
+                                          shuttle.Height / 2),
+                              SpriteEffects.None, 1f);
+            */
             
             _spriteBatch.DrawString(font,
                                     "Ola Mundo!",
@@ -91,7 +138,8 @@ namespace Game1Game
                                     0,
                                     centroTexto,
                                     1.0f,
-                                    SpriteEffects.None,0.5f);
+                                    SpriteEffects.None,
+                                    0.5f);
             
             
             _spriteBatch.End();
